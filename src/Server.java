346b3cc253024dec8094674ef2539398c4532573
Margarita Fadeeva <morganGTO@yandex.ru>
2022-03-26 21:04:36 +0300

    Init

---
+++ src/Server.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        final ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.bind(new InetSocketAddress("localhost", 9999));
+
+        while (true) {
+            try (SocketChannel socketChannel = serverChannel.accept()) {
+                final ByteBuffer inputBuffer = ByteBuffer.allocate(2 << 10);
+                while (socketChannel.isConnected()) {
+                    int bytesCount = socketChannel.read(inputBuffer);
+                    if (bytesCount == -1) break;
+                    final String msg = new String(inputBuffer.array(), 0, bytesCount,
+                            StandardCharsets.UTF_8);
+                    inputBuffer.clear();
+                    System.out.println("Получено сообщение от клиента: " + msg);
+                    socketChannel.write(ByteBuffer.wrap((deleteSpaces(msg)).getBytes(StandardCharsets.UTF_8)));
+                }
+            } catch (IOException err) {
+                System.out.println(err.getMessage());
+            }
+        }
+    }
+
+    public static String deleteSpaces(String string) {
+        return string.replace(" ", "");
+    }
+}
